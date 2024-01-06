fa66f5d056e5f6d02758b48c27cff2c06c7f8d00
emess2g <essuonemma84@gmail.com>
2024-01-06 23:54:22 +0000

    documented code update

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 let popup = document.getElementById("popup");
 
+// displays hidden popup
 function openPopup(){
     popup.classList.add("open-popup")
 }
 
+// closes popup
 function closePopup(){
     popup.classList.remove("open-popup")
 }
